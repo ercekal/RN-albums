3646af6f4a9e500edb3219778df67e41ce9c7e17
ercekal <ercekalabalikoglu@gmail.com>
2017-01-03 15:32:04 +0000

    adds styling to header

---
+++ src/components/header.js
@@ -0,0 +1,27 @@
+// Import libraries
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// make a component
+const Header = () => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums</Text>
+    </View>
+  );
+};
+
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  },
+  viewStyle: {
+    backgroundColor: '#F8F8F8'
+  }
+};
+
+// make the component available to other parts of the app
+export default Header;
